bea380253414a817a711586c14f630f61903ff57
Chad Austin <aegis@aegisknight.org>
2009-11-30 21:35:06 -0800

    deploy to website

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd "`dirname \"$0\"`"
+
 die() {
     echo failed
     exit 1
@@ -8,5 +10,8 @@ die() {
 TODAY=`date +%Y-%m-%d`
 
 C:/Python31/python C:/Python31/Scripts/cxfreeze src/ibb.py --target-name=ibb_server.exe --target-dir=dist || die
+cp ibb.exe dist/ibb.exe || die
 rm -f ibb-$TODAY.zip || die
 zip -r ibb-$TODAY.zip dist || die
+
+scp ibb-$TODAY.zip chad@chadaustin.xen.prgmr.com:/home/chad/public_html/ibb || die
